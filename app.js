d83f98810590e65d6fdfc3692e25810d016abc2d
bspwd <128273275+bspwd@users.noreply.github.com>
2023-05-13 15:40:56 +0700

    Learn Node.js : Lecture 22 Manage Route

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require('express');
 const debug = require('debug')('app');
 const morgan = require ('morgan');
 const path = require('path');
-const products = require("./data/products.json");
-const productRouter = express.Router();
 
 const app = express();
 const port = process.env.PORT;
+const productsRouter = require("./src/router/productsRouter");
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")));  //กำหนด Path หน้าเว็บไซต์
@@ -14,18 +13,7 @@ app.use(express.static(path.join(__dirname,"/public/")));  //กำหนด Pat
 app.set("views","./src/views");
 app.set("view engine", "ejs")
 
-productRouter.route("/").get((req,res) => {
-    res.render("products",
-        products,
-    );
-
-});
-
-productRouter.route("/1").get((req,res) => {
-    res.render("products");
-});
-
-app.use("/products", productRouter)
+app.use("/products", productsRouter)
 
 app.get("/",(req,res) =>{
 
